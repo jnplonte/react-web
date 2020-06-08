fc7659fe6f7828146c82db91e9d2e77fc6c8e8d3
John Paul Onte <jnpl.onte@gmail.com>
2020-06-08 09:11:08 +0800

    server low memory fix

---
+++ build/precache-manifest.e5a573c5f9ebb37000f6e0749c27c63b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b8e879907991a25f748",
+    "url": "/static/js/0.9b8e8799.chunk.js"
+  },
+  {
+    "revision": "999881ce8d33c3f3c690",
+    "url": "/static/css/main.584ccf5c.chunk.css"
+  },
+  {
+    "revision": "999881ce8d33c3f3c690",
+    "url": "/static/js/main.999881ce.chunk.js"
+  },
+  {
+    "revision": "3fb5214deef6a3c88070",
+    "url": "/static/js/2.3fb5214d.chunk.js"
+  },
+  {
+    "revision": "823a29dfa4eecd96cfcf",
+    "url": "/static/js/3.823a29df.chunk.js"
+  },
+  {
+    "revision": "ee97c160bafe50874f46",
+    "url": "/static/js/4.ee97c160.chunk.js"
+  },
+  {
+    "revision": "1b4add3b5b6e64ccc514",
+    "url": "/static/js/5.1b4add3b.chunk.js"
+  },
+  {
+    "revision": "3250273ff1422df71953",
+    "url": "/static/js/6.3250273f.chunk.js"
+  },
+  {
+    "revision": "7efd9702134794ef28a2",
+    "url": "/static/js/7.7efd9702.chunk.js"
+  },
+  {
+    "revision": "6840e701c04ac1067002",
+    "url": "/static/js/8.6840e701.chunk.js"
+  },
+  {
+    "revision": "a7560b47c0b85160d7e6",
+    "url": "/static/js/9.a7560b47.chunk.js"
+  },
+  {
+    "revision": "d74a99c1f8f977ad42f3",
+    "url": "/static/js/10.d74a99c1.chunk.js"
+  },
+  {
+    "revision": "301fd8e093af7e49bf88",
+    "url": "/static/js/11.301fd8e0.chunk.js"
+  },
+  {
+    "revision": "0627776c2a802e178e81",
+    "url": "/static/js/12.0627776c.chunk.js"
+  },
+  {
+    "revision": "5a5614f2a744b876e807",
+    "url": "/static/js/13.5a5614f2.chunk.js"
+  },
+  {
+    "revision": "06c4d02d029bf850e1ac",
+    "url": "/static/js/runtime~main.06c4d02d.js"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "ec4c9962ba54eb91787aa93d361c10a8",
+    "url": "/static/media/Roboto-Black.ec4c9962.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "50705c5ed1205b63efdbfee941a6b655",
+    "url": "/static/media/Roboto-BlackItalic.50705c5e.ttf"
+  },
+  {
+    "revision": "42bbe4eefcde1297b11dc4b6491e9746",
+    "url": "/static/media/Roboto-Italic.42bbe4ee.ttf"
+  },
+  {
+    "revision": "1eb7a893589ddce89d81cdb22a356660",
+    "url": "/static/media/Roboto-BoldItalic.1eb7a893.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "d1efcd4d126837fe0dcf9b6cf3a00d64",
+    "url": "/static/media/Roboto-LightItalic.d1efcd4d.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "bd19ad60600a1537c00d3b4923a5e5de",
+    "url": "/static/media/Roboto-MediumItalic.bd19ad60.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "0fc25386220a58203994ce45fb4ae570",
+    "url": "/static/media/Roboto-ThinItalic.0fc25386.ttf"
+  },
+  {
+    "revision": "bf20f75e585de1415e9d4c2ce9206bfd",
+    "url": "/index.html"
+  }
+];
